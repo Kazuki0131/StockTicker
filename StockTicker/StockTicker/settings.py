b6f4f2c4e5fbe6a188a8046000b2e377fa4a3275
Kazuki Takehashi <ferry.oh@gmail.com>
2022-08-08 11:16:21 -0700

    Update settings.py

--- StockTicker/StockTicker/settings.py
+++ StockTicker/StockTicker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ab*tpxwhej9824p7%bf0(c)-fsgtyedkw!u189r2k5#nx9=s-n"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
